56c450665a75607a661d4ccf7f47163cb9cc0e2b
isadora-santos-rtgit config --global user.email <20231ireinfint0003@ifba.edu.br>
2026-09-07 16:12:59 -0300

    Criacao da API AccessLab

---
+++ acesslab/src/main/java/acesslab/AcesslabApplication.java
@@ -0,0 +1,13 @@
+package acesslab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcesslabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcesslabApplication.class, args);
+	}
+
+}
